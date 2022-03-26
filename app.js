d95829556eb343f43ff3086f03aeeda6cbb4a885
Saidov Yosin <saidovyosin2004@gmail.com>
2022-03-26 16:25:55 +0300

    ChanhgeSvatafor

---
+++ app.js
@@ -0,0 +1,34 @@
+const container = document.querySelector(".container") 
+
+const item = document.querySelector(".item") 
+
+const block = document.querySelector(".block") 
+
+function changeValue(elem, value){
+   elem.innerHTML = value
+   console.log("value")
+}
+
+function switchon(item){
+    item.style.opacity = "100%"
+for(let i=0; i<3; i++){
+    setTimeout(changeValue, i*1000, item, 3-i)
+}
+} 
+
+function switchoff(item){
+    item.style.opacity = "20%"
+   item.innerHTML = ""
+}
+
+function run (){
+    setTimeout(switchon, 0, container)
+    setTimeout(switchoff, 3000, container)
+    setTimeout(switchon, 3000, item)
+    setTimeout(switchoff, 6000, item)
+    setTimeout(switchon, 6000, block)
+    setTimeout(switchoff, 9000, block)
+}
+run()
+setInterval(run, 9000)
+
